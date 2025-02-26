shravansayz/frameworks_base_ten

SystemUI: !fixup Ambient display show battery & cpu temperature
Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -1102,7 +1102,7 @@ public class KeyguardIndicationController {
             } else {
                 String batteryLevel = NumberFormat.getPercentInstance().format(mBatteryLevel / 100f);
                 String batteryTemp = com.android.internal.util.tenx.Utils.batteryTemperature(mContext, false);
-                String cpuTemp = com.android.internal.util.tenx.Utils.batteryTemperature(mContext, false);
+                String cpuTemp = com.android.internal.util.tenx.Utils.getCPUTemp(mContext);
 
                 Drawable batteryIcon = mContext.getDrawable(R.drawable.ic_ambient_battery);
                 Drawable cpuIcon = mContext.getDrawable(R.drawable.ic_ambient_cpu);

Addy-95/Matrixx_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -1161,7 +1161,7 @@ public class KeyguardIndicationController {
             } else {
                 String batteryLevel = NumberFormat.getPercentInstance().format(mBatteryLevel / 100f);
                 String batteryTemp = com.android.internal.util.crdroid.Utils.batteryTemperature(mContext, false);
-                String cpuTemp = com.android.internal.util.crdroid.Utils.batteryTemperature(mContext, false);
+                String cpuTemp = com.android.internal.util.crdroid.Utils.getCPUTemp(mContext);
 
                 Drawable batteryIcon = mContext.getDrawable(R.drawable.ic_ambient_battery);
                 Drawable cpuIcon = mContext.getDrawable(R.drawable.ic_ambient_cpu);